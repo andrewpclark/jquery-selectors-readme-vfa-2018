3acc33076b4b948e34d8b0b3499c6918b8f5dfc9
Andrew Clark <a95clark@gmail.com>
2018-06-26 18:44:39 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,10 +1,22 @@
 // declare your functions here...
-$('selector-goes-here')
-$('img')
-$('.pics')
-$('#baby-ninja')
-$('ul li')
-$('parent-tag child-tag:first-child')
-$('div img:first-child')
-$("img[alt='the beatles making faces']")
-$('div:last')
+
+
+function paragraphSelector(){
+  return $("p");
+}
+
+function lastImageSelector(){
+  return $("img:last");
+}
+
+function ninjaBabySelector(){
+  return $("#baby-ninja");
+}
+
+function divSelector(){
+  return $(".pics");
+}
+
+function firstListItem(){
+  return $("ul li:first-child");
+}
